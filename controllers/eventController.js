2dfa091a652ee29c448b67cc5fc2e37d19978afb
AbimaelQM <abimaelqm@hotmail.com>
2023-05-27 17:35:23 -0500

    Minor Up. user update correct

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -2,31 +2,48 @@ const db = require('../models');
 const Event = db.Event;
 
 async function createEvent(req, res) {
-    try {
-  
-      console.log(req.user);
-      
-      if (req.user.role !== 'ADMIN') {
-          return res.status(403).json({ message: 'Você não tem permissão para criar eventos.' });
-      }
-  
-      const { name, state, date, location, description } = req.body;
-  
-      const newEvent = await Event.create({
-        name,
-        state,
-        date,
-        location,
-        description,
-      });
-  
-      res.status(201).json({ event: newEvent });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Erro ao criar o evento' });
+  try {
+
+    console.log(req.user);
+
+    if (req.user.role !== 'ADMIN') {
+      return res.status(403).json({ message: 'Você não tem permissão para criar eventos.' });
     }
+
+    const { name, state, date, location, description } = req.body;
+
+    const newEvent = await Event.create({
+      name,
+      state,
+      date,
+      location,
+      description,
+    });
+
+    res.status(201).json({ event: newEvent });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erro ao criar o evento' });
   }
-  
+}
+
+
+async function getEvent(req, res) {
+  try {
+    const eventId = req.params.id;
+
+    const event = await Event.findByPk(eventId);
+
+    if (!event) {
+      return res.status(404).json({ error: 'Evento não encontrado' });
+    }
+
+    res.json({ event });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erro ao obter o evento' });
+  }
+}
 
 async function getEvent(req, res) {
   try {
@@ -49,27 +66,37 @@ async function updateEvent(req, res) {
   try {
 
     if (req.user.role !== 'ADMIN') {
-        return res.status(403).json({ message: 'Você não tem permissão editar evento.' });
+      return res.status(403).json({ message: 'Você não tem permissão editar evento.' });
     }
 
     const eventId = req.params.id;
-    const { name, state, date, location, description } = req.body;
-
+    // const { name, state, date, location, description } = req.body;
+    
     const event = await Event.findByPk(eventId);
-
+    
     if (!event) {
       return res.status(404).json({ error: 'Evento não encontrado' });
     }
+    
+    // event.name = name;
+    // event.state = state;
+    // event.date = date;
+    // event.location = location;
+    // event.description = description;
+    
+    // await event.save();
 
-    event.name = name;
-    event.state = state;
-    event.date = date;
-    event.location = location;
-    event.description = description;
+    const eventUp = req.body;
+    await Event.update(eventUp,
+      {
+        where: { id: eventId }
+      }
+    )
 
-    await event.save();
 
-    res.json({ event });
+    const updated = await Event.findByPk(eventId);
+
+      res.json(updated);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Erro ao atualizar o evento' });
@@ -80,7 +107,7 @@ async function deleteEvent(req, res) {
   try {
 
     if (req.user.role.name !== 'ADMIN') {
-        return res.status(403).json({ message: 'Você não tem permissão para deletar eventos.' });
+      return res.status(403).json({ message: 'Você não tem permissão para deletar eventos.' });
     }
 
     const eventId = req.params.id;
--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/authMiddleware.js')
-const { createEvent, updateEvent, deleteEvent } = require('../controllers/eventController.js');
+const { createEvent, updateEvent, deleteEvent, getEvent } = require('../controllers/eventController.js');
 
 router.post('/event', authMiddleware, createEvent);
-router.put('/event', authMiddleware, updateEvent);
+router.get('/event/:id', authMiddleware, getEvent);
+router.put('/event/:id', authMiddleware, updateEvent);
 router.delete('/event', authMiddleware, deleteEvent);
 
 module.exports = router;

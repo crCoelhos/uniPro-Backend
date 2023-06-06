79785b916ccc364703f360999eb995352917ac40
ppaalo <ppaalo.henriquecosta@gmail.com>
2023-06-06 17:46:33 -0300

    Payment test

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -3,10 +3,15 @@ const Ticket = db.Ticket;
 const Category = db.Category;
 const User = db.User;
 const User_ticket = db.User_ticket;
+const mercadopago = require('mercadopago');
 const jwt = require('jsonwebtoken');
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
 
+// Configurar o token de acesso do Mercado Pago
+mercadopago.configurations.setAccessToken(config.mercadopago.access_token);
+
+// levar por ticket controller (necessario)
 async function bookTicket(req, res) {
   try {
     const authHeader = req.headers.authorization;
@@ -56,6 +61,62 @@ async function bookTicket(req, res) {
   }
 }
 
+async function ProcessPayment(req, res) {
+  try {
+    const { body } = req;
+    const { payer } = body;
+
+    // Validate the input data
+    if (!Number.isInteger(body.transactionAmount)) {
+      throw new Error("Invalid transaction amount");
+    }
+
+    if (!body.token) {
+      throw new Error("Missing token");
+    }
+
+    // Create the payment data
+    const paymentData = {
+      transaction_amount: body.transactionAmount,
+      token: body.token,
+      description: body.description,
+      installments: body.installments,
+      payment_method_id: body.paymentMethodId,
+      issuer_id: body.issuerId,
+      payer: {
+        email: payer.email,
+        identification: {
+          type: payer.identification.docType,
+          number: payer.identification.docNumber
+        }
+      }
+    };
+
+    // Call the Mercadopago API
+    const response = await mercadopago.payment.save(paymentData);
+    const { response: data } = response;
+
+    // Check the status of the payment
+    if (data.status === "approved") {
+      res.status(201).json({
+        detail: data.status_detail,
+        status: data.status,
+        id: data.id
+      });
+    } else {
+      res.status(400).json({
+        error_message: data.status_detail
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      error_message: error.message
+    });
+  }
+}
+
 module.exports = {
-  bookTicket
+  bookTicket,
+  ProcessPayment
 };

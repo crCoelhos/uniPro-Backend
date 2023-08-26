0f31725c2ce930d17e20520462f3f8c36a250621
ppaalo <60149992+ppaalo@users.noreply.github.com>
2023-08-26 16:56:32 -0500

    Minor up. adapation fom pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    apps: [
+      {
+        name: "back",
+        script: "yarn",
+        args: "run start",
+        watch: true,
+        interpreter: "bash",
+      },
+    ],
+  };  

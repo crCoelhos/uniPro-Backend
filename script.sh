e3a755316cf6716d31c106ae132e7f8284ca9f0c
ppaalo <60149992+ppaalo@users.noreply.github.com>
2023-08-26 22:03:41 -0500

    Minor up. Adaption

--- script.sh
+++ script.sh
@@ -2,11 +2,10 @@
 
 # Define a pasta do repositório
 REPO_FOLDER=/home/api/htdocs/api.uniproducoes.com.br
-current_date=$(date +"-%d-%m-%Y %H:%M:%S")
 
 cd $REPO_FOLDER
 
-git fetch origin main
+git pull origin main
 
 if git rev-parse HEAD..origin/main >/dev/null 2>&1; then
   echo "Nova atualização na main. Executando comando..."
@@ -15,7 +14,9 @@ if git rev-parse HEAD..origin/main >/dev/null 2>&1; then
   yarn run migration
   pm2 restart back
   
-  echo "Executado em: $current_date" >> /home/api/script_log.txt
+  current_date=$(date +"-%d-%m-%Y %H:%M:%S")
+  echo "Executado em: $current_date" >> /home/front/script_log.txt
 else
-  echo "$current_date Não há novas atualizações na main" >> /home/api/script_log.txt
+  current_date=$(date +"-%d-%m-%Y %H:%M:%S")
+  echo "$current_date Não há novas atualizações na main" >> /home/front/script_log.txt
 fi

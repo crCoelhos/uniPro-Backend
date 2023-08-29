d0fde5b39d40e490569352e5d6d3e09c34e5ffd9
crCoelhosm <cristophersaar@gmail.com>
2023-08-29 13:06:11 -0500

    Minor up. TransationController and model fixing

--- controllers/transationController.js
+++ controllers/transationController.js
@@ -3,9 +3,8 @@ const Transation = db.Transation;
 
 async function createTransation(req, res) {
     try {
-
         const transation = req.body;
-        const newTransation = await Transation.create(transation)
+        const newTransation = await Transation.create(transation);
 
         res.status(201).json(newTransation);
     } catch (err) {
@@ -14,23 +13,20 @@ async function createTransation(req, res) {
 }
 
 async function getAllTransations(req, res) {
-
     try {
         if (req.user.role !== 'ADMIN') {
             return res.status(403).json({ message: 'Você não tem permissão para ver as transações.' });
         }
 
-        const transation = await Transation.findAll()
+        const transations = await Transation.findAll();
 
-        res.status(200).json(transation)
-    }
-    catch (err) {
-        res.status(500).json({ message: err.message })
+        res.status(200).json(transations);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
     }
 }
-
 
 module.exports = {
     createTransation,
     getAllTransations
-}
+};

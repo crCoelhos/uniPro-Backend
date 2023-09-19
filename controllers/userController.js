bd6795ce9a5277e9dd9c7de4028f67e44b5ee214
crCoelhosm <cristophersaar@gmail.com>
2023-09-19 01:22:25 -0500

    disgraça

--- controllers/userController.js
+++ controllers/userController.js
@@ -44,7 +44,7 @@ async function getAllUsers(req, res) {
             {
                 model: Ticket,
                 as: 'ticket',
-                attributes: ['name'],
+                attributes: ['name', 'createdAt'],
             }
             ],
             attributes: {
--- seeders/20230617230057-demo-typeTicket.js
+++ seeders/20230617230057-demo-typeTicket.js
@@ -4,42 +4,48 @@
 module.exports = {
   async up (queryInterface, Sequelize) {
     return queryInterface.bulkInsert('Types_tickets', [
-      {
-        name:"Pacote Bronze",
-        qt_modalities: 1,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        name:"Pacote Prata",
-        qt_modalities: 4,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        name:"Pacote Ouro",
-        qt_modalities: 6,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        name:"Pacote Diamante",
-        qt_modalities: 14,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        name:"Torcedor Simples",
-        qt_modalities: 14,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        name:"Torcedor Completo",
-        qt_modalities: 14,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
+      // {
+      //   name:"Pacote Bronze",
+      //   qt_modalities: 1,
+      //   createdAt: new Date(),
+      //   updatedAt: new Date()
+      // },
+      // {
+      //   name:"Pacote Prata",
+      //   qt_modalities: 4,
+      //   createdAt: new Date(),
+      //   updatedAt: new Date()
+      // },
+      // {
+      //   name:"Pacote Ouro",
+      //   qt_modalities: 6,
+      //   createdAt: new Date(),
+      //   updatedAt: new Date()
+      // },
+      // {
+      //   name:"Pacote Diamante",
+      //   qt_modalities: 14,
+      //   createdAt: new Date(),
+      //   updatedAt: new Date()
+      // },
+      // {
+      //   name:"Torcedor Simples",
+      //   qt_modalities: 14,
+      //   createdAt: new Date(),
+      //   updatedAt: new Date()
+      // },
+      // {
+      //   name:"Torcedor Completo",
+      //   qt_modalities: 14,
+      //   createdAt: new Date(),
+      //   updatedAt: new Date()
+      // },
+      // {
+      //   name:"Torcedor",
+      //   qt_modalities: 0,
+      //   createdAt: new Date(),
+      //   updatedAt: new Date()
+      // },
       {
         name:"Torcedor",
         qt_modalities: 0,
